1c6a719feb14c65523ac6b3712e49c449f91c4d0
Domitilla1234 <162117092+Domitilla1234@users.noreply.github.com>
2024-03-04 16:01:41 +0000

    ciaooo pier

---
+++ esercizio00.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+
+         printf("piergiorgiocamorrista\n");
+         return(0);
+}
---
+++ esercizio03.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int p=17;
+    int d=30;
+    int c=p+d;
+    c=p*d;
+    printf("%d",c);
+}
